93e1eddac84d9c263e4126fb7df0045638b18089
Carloxxz <carloxmz@gmail.com>
2024-05-01 21:08:48 -0600

    -Usa enums

--- playground.ts
+++ playground.ts
@@ -7,3 +7,12 @@ let x: number
 // let z
 // z  = "hola3"
 // z =  true
+
+enum ContractStatus {
+    Permant, // 0
+    Temp, // 1
+    Apprentice // 2
+}
+
+const employeeStatus: ContractStatus = ContractStatus.Temp
+console.log(employeeStatus) // 1
