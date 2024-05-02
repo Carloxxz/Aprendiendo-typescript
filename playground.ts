a85db820211fefd81e4c6f6c82d78792debb0499
Carloxxz <carloxmz@gmail.com>
2024-05-01 22:39:47 -0600

    - Agrega tipos de unión

--- playground.ts
+++ playground.ts
@@ -9,10 +9,54 @@ let x: number
 // z =  true
 
 enum ContractStatus {
-    Permant, // 0
-    Temp, // 1
-    Apprentice // 2
+    Permant = 1, // 0 y puedes añadirle que empiece con otro valor, ahora es 1
+    Temp, // 1 ahora es 2
+    Apprentice // 2 ahora es 3
 }
 
 const employeeStatus: ContractStatus = ContractStatus.Temp
-console.log(employeeStatus) // 1
+// console.log(employeeStatus) // 1
+console.log(ContractStatus[employeeStatus]);
+
+let randomValue: unknown = 10
+randomValue = true
+randomValue = 'Carlos'
+
+    // Con any si funciona pero con unknow NO
+    // randomValue()
+    // randomValue.name
+    // randomValue.getName()
+
+
+    // estas dos lineas son iguales, fuerzan el tipo de variable "Aserción de tipos"
+    ; (randomValue as string).toUpperCase();
+(<string>randomValue).toUpperCase();
+
+// restricción de tipos (typeof y Array.isArray)
+if (typeof randomValue === 'string') {
+    randomValue.toLocaleLowerCase()
+}
+
+// tipos de unión --> number | boolean mas de un tipo que puede tener
+let multiType: number | string = 20
+multiType = 'veinte'
+
+// arriba funciona, aqui NO
+let type: number = 20
+type = 'number'
+
+function add(x: number | string, y: number | string) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x + y // funciona
+    }
+
+    if (typeof x === 'string' && typeof y === 'string') {
+        return x.concat(y)
+    }
+
+    throw new Error('You\'re mixing types!')
+}
+
+add(1, 2)  // 3
+add('one', 'two') // "onetwo"
+add(1, "two") // error
