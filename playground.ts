a95972639b09732be9a79dcf1feca31149af7520
Carloxxz <carloxmz@gmail.com>
2024-05-02 21:05:12 -0600

    - Agrega interfaz en una API

--- playground.ts
+++ playground.ts
@@ -98,3 +98,79 @@ interface Employee {
     lastName: string;
     fullName(): string;
 }
+
+let employe: Employee = {
+    firstName: "Emil",
+    lastName: "Andersson",
+    fullName(): string {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+employee.firstName = 10;  //* Error - Type 'number' is not assignable to type 'string'
+
+
+// Ejemplo
+
+interface IceCream {
+    flavor: string;
+    scoops: number;
+}
+
+function tooManyScoops(dessert: IceCream) {
+    if (dessert.scoops >= 4) {
+        return dessert.scoops + ' is too many scoops!';
+    } else {
+        return 'Your order will be ready soon!';
+    }
+}
+
+console.log(tooManyScoops({ flavor: 'vanilla', scoops: 5 }));
+
+// Extensión de una interfaz (? aignifica opcional)
+interface Sundae extends IceCream {
+    sauce: 'chocolate' | 'caramel' | 'strawberry';
+    nuts?: boolean;
+    whippedCream?: boolean;
+    instructions?: boolean;
+}
+
+// Tipos indexables
+
+interface IceCreamArray {
+    [index: number]: string;
+}
+
+// la interfaz IceCreamArray declara una signatura de índice como number y devuelve un tipo string. Esta signatura de índice indica que la interfaz IceCreamArray está indexada con un número y devolverá una cadena.
+let myIceCream: IceCreamArray;
+myIceCream = ['chocolate', 'vanilla', 'strawberry'];
+let myStr: string = myIceCream[0];
+console.log(myStr);
+
+// Interfaz en una API
+
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts'
+// Interface describing the shape of our json data
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+async function fetchPosts(url: string) {
+    let response = await fetch(url);
+    let body = await response.json();
+    return body as Post[];
+}
+async function showPost() {
+    let posts = await fetchPosts(fetchURL);
+    // Display the contents of the first item in the response
+    let post = posts[0];
+    console.log('Post #' + post.id)
+    // If the userId is 1, then display a note that it's an administrator
+    console.log('Author: ' + (post.userId === 1 ? "Administrator" : post.userId.toString()))
+    console.log('Title: ' + post.title)
+    console.log('Body: ' + post.body)
+}
+
+showPost();
