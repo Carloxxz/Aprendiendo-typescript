4b81be33f5b4d90a0c8c48169d89a6d9dc60dedc
Carloxxz <carloxmz@gmail.com>
2024-05-03 19:27:23 -0600

    - Agrega temas de clases

--- playground.ts
+++ playground.ts
@@ -201,11 +201,38 @@ addNumber(1);    // Returns 1
 
 class Car {
     // Properties
-
+    _make: string;
+    _color: string;
+    _doors: number;
     // Constructor
-
+    constructor(make: string, color: string, doors = 4) {
+        this._make = make;
+        this._color = color;
+        this._doors = doors;
+    }
     // Accessors
-
+    get doors() {
+        return this._doors;
+    }
+    set doors(doors) {
+        if ((doors % 2) === 0) {
+            this._doors = doors;
+        } else {
+            throw new Error('Doors must be an even number');
+        }
+    }
     // Methods
-
+    accelerate(speed: number): string {
+        return `${this.worker()} is accelerating to ${speed} MPH.`
+    }
+    brake(): string {
+        return `${this.worker()} is braking with the standard braking system.`
+    }
+    turn(direction: 'left' | 'right'): string {
+        return `${this.worker()} is turning ${direction}`;
+    }
+    // This function performs work for the other method functions
+    worker(): string {
+        return this._make;
+    }
 }
